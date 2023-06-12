f282ec6a4e7251e91c3beab1be98726781a0f4d8
pttkachuk <petrotkachuk6@gmail.com>
2023-06-12 16:16:25 +0200

    done second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.on('timeupdate', throttle(playOn, 1000));
+
+function playOn({ seconds }) {
+    localStorage.setItem('videoplayer-current-time', seconds)
+};
+
+function setCurrentTime() {
+    if (!localStorage.getItem('videoplayer-current-time')) {
+        return;
+    }
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+setCurrentTime();
