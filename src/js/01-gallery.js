a03486478bd501f9918e35e81daa34cb21c30182
pttkachuk <petrotkachuk6@gmail.com>
2023-06-12 16:15:02 +0200

    done first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-console.log(object);
+const cardList = document.querySelector(".gallery");
+
+function createCardList(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    ).join('');
+}
+cardList.insertAdjacentHTML('beforeend', createCardList(galleryItems));
+
+new SimpleLightbox('.gallery__link', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
